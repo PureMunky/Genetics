eaf631e44c78abad7d214edad703c1fa4a94793d
Phil Corbett <corbett.phil@gmail.com>
2018-10-09 17:42:09 +0000

    update the server file

--- server.js
+++ server.js
@@ -3,11 +3,10 @@
 const express = require('express');
 
 const PORT = 8080;
-const HOST = 'localhost';
 const STATIC_FOLDER = 'public';
 const app = express();
 
 app.use(express.static(STATIC_FOLDER));
 
 app.listen(PORT);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on http://localhost:${PORT}`);
